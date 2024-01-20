9bd8a52a4664f8da3943bd560064203ff50f6ff3
Amirjoin <amirjonkarimoff@gmail.com>
2024-01-20 21:32:46 +0500

    add deploy

--- data/config.py
+++ data/config.py
@@ -7,6 +7,7 @@ env.read_env()
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+IP = env.str('IP')
 DB_USER = env.str("DB_USER")
 DB_PASS = env.str("DB_PASS")
 DB_NAME = env.str("DB_NAME")
