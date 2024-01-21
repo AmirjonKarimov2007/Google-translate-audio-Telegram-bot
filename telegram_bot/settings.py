15a6802ca786f76e4baa1a121110d45778a4741a
Amirjoin <amirjonkarimoff@gmail.com>
2024-01-21 14:51:39 +0500

    ma

--- telegram_bot/settings.py
+++ telegram_bot/settings.py
@@ -130,10 +130,11 @@ import os
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / 'home/amirjon/Desktop/Instagram-Downloader-Telegram-Bot/static',
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
