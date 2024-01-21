9ae28f1f091e43a13cd98ae64142d8733da0c4f9
Amirjoin <amirjonkarimoff@gmail.com>
2024-01-21 14:48:51 +0500

    ma

--- telegram_bot/settings.py
+++ telegram_bot/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e8s0w-j9fp99kbpfg&sf#1&qydzbejlq4d23gth1b-75imrs11'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -128,11 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 import os
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / '/home/amirjon/Desktop/Instagram-Downloader-Telegram-Bot/static'
+    BASE_DIR / 'home/amirjon/Desktop/Instagram-Downloader-Telegram-Bot/static',
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
