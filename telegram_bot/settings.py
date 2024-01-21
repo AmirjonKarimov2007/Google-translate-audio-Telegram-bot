5f3b1c2d27985da1ee14bcb1dc41b382a2d3cfe6
Amirjoin <amirjonkarimoff@gmail.com>
2024-01-21 13:20:20 +0500

    ad

--- telegram_bot/settings.py
+++ telegram_bot/settings.py
@@ -129,10 +129,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIR = [
-    BASE_DIR / 'static'
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
