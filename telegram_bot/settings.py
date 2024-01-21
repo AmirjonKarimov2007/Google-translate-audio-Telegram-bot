e9c7562a84c802633ff6008793fa0a7c52d4bcbc
Amirjoin <amirjonkarimoff@gmail.com>
2024-01-21 13:50:19 +0500

    deploy

--- telegram_bot/settings.py
+++ telegram_bot/settings.py
@@ -126,11 +126,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
-import os
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
+MEDIA_URL = 'media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
