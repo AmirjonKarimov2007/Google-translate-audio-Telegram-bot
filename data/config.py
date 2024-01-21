280535464986197670ffc4512620d9e741ea4aae
Amirjoin <amirjonkarimoff@gmail.com>
2024-01-21 10:01:29 +0500

    blablabla

--- data/config.py
+++ data/config.py
@@ -5,7 +5,7 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+BOT_TOKEN = '6267733278:AAGiVkawajxs3nx3OdwFMRa0LLUOsSaUJ7E'  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str('IP')
 DB_USER = env.str("DB_USER")
